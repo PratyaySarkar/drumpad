10f2acb95754bd84bcbe579a06e02d57821ec51a
PraSar <55887682+PratyaySarkar@users.noreply.github.com>
2022-06-01 01:18:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,38 +15,38 @@ function onKey(key) {
     console.log(key);
     switch (key) {
         case 'a':
-            var aud = new Audio("/sounds/tom-1.mp3");
+            var aud = new Audio("./sounds/tom-1.mp3");
             aud.play();
             console.log("click");
             break;
 
         case 's':
-            var aud = new Audio("/sounds/tom-3.mp3");
+            var aud = new Audio("./sounds/tom-3.mp3");
             aud.play();
             break;
 
         case 'd':
-            var aud = new Audio("/sounds/tom-4.mp3");
+            var aud = new Audio("./sounds/tom-4.mp3");
             aud.play();
             break;
 
         case 'f':
-            var aud = new Audio("/sounds/tom-2.mp3");
+            var aud = new Audio("./sounds/tom-2.mp3");
             aud.play();
             break;
 
         case 'j':
-            var aud = new Audio("/sounds/snare.mp3");
+            var aud = new Audio("./sounds/snare.mp3");
             aud.play();
             break;
 
         case 'k':
-            var aud = new Audio("/sounds/kick-bass.mp3");
+            var aud = new Audio("./sounds/kick-bass.mp3");
             aud.play();
             break;
 
         case 'l':
-            var aud = new Audio("/sounds/crash.mp3");
+            var aud = new Audio("./sounds/crash.mp3");
             aud.play();
             break;
 
